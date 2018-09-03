8b562d24867536cbf970c35a02d75478a1881351
Adam Sharp <adam@sharplet.me>
2018-09-03 15:07:15 -0400

    Set up CORS and Fluent commands

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,14 +5,27 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
   /// Register providers first
   try services.register(FluentSQLiteProvider())
 
+  services.register { container -> CommandConfig in
+    var config = CommandConfig.default()
+    config.useFluentCommands()
+    return config
+  }
+
   /// Register routes to the router
   let router = EngineRouter.default()
   try routes(router)
   services.register(router, as: Router.self)
 
+  // CORS config
+  let cors = CORSMiddleware(configuration: .init(
+    allowedOrigin: .all, // FIXME: not in production!
+    allowedMethods: [.GET, .POST, .DELETE, .OPTIONS, .PATCH],
+    allowedHeaders: [.xRequestedWith, .origin, .contentType, .accept]
+  ))
+
   /// Register middleware
   var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-  /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+  middlewares.use(cors)
   middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
   services.register(middlewares)
 
